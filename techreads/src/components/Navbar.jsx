50797320a88fe8a53ba97d05def11318db8cb172
Dennis Kemboi <denkogei11@gmail.com>
2025-02-21 14:50:02 +0300

    added signup

--- techreads/src/components/Navbar.jsx
+++ techreads/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           <span className="text-blue-900 text-4xl">📖</span> 
           <h1 className="text-2xl font-semibold cursor-pointer hover:text-blue-600">TechReads</h1>
         </Link>
-        <Link to="/books" className="text-lg font-medium hover:text-blue-600">All Books</Link>
+        <Link to="/signup" className="text-lg font-medium hover:text-blue-600">All Books</Link>
       </div>
 
       
