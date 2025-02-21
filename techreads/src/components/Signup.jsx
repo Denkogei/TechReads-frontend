c05ac7307760927c63d1345f8cb4d64a8aac1ff4
Mary Monchari Gichana <marygichana95@gmail.com>
2025-02-21 13:28:38 +0300

    added handleSubmit function in Signup component

--- techreads/src/components/Signup.jsx
+++ techreads/src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 function Signup() {
@@ -20,6 +20,26 @@ function Signup() {
       .min(6, "Password must be at least 6 characters")
       .required("Password is required"),
   });
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    fetch("http://127.0.0.1:5555/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.success) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        alert("Signup successful!");
+        resetForm();
+      })
+      .catch((err) => alert(err));
+
+    setSubmitting(false);
+  };
+
   return (
     <>
       <div>Signup</div>
