82a5690fc8e4935c563e8c97f4179fdffca62516
Mary Monchari Gichana <marygichana95@gmail.com>
2025-02-21 13:13:54 +0300

    Updated Signup form validation schema

--- techreads/src/components/Signup.jsx
+++ techreads/src/components/Signup.jsx
@@ -3,6 +3,23 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 function Signup() {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .min(3, "Must be at least 3 characters")
+      .required("Username is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
   return (
     <>
       <div>Signup</div>
