44f6e1059d976a22c0c00c42fed004b1fe1d96b2
Dennis Kemboi <denkogei11@gmail.com>
2025-02-22 15:35:40 +0300

    Google authentication

--- techreads/src/components/Homepage.jsx
+++ techreads/src/components/Homepage.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="bg-blue-600 text-white rounded-2xl py-12 px-10 max-w-7xl mx-auto w-full flex flex-col items-start">
         <h1 className="text-3xl font-bold">Welcome to TechReads</h1>
         <p className="mt-2 text-base">Empowering Kenyan Minds</p>
-        <Link to="/books">
+        <Link to="/login">
           <button className="mt-4 bg-white text-blue-600 px-5 py-2.5 rounded-lg font-medium hover:bg-gray-100">
             Browse Books
           </button>
