dcd534092b6a801a54a893f36a5ab98918e5515f
Mary Monchari Gichana <marygichana95@gmail.com>
2025-02-24 19:11:49 +0300

    Implement book fetching and state management in AllBooks component

--- techreads/src/components/AllBooks.jsx
+++ techreads/src/components/AllBooks.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AllBooks() {
+  const [books, setBooks] = useState([]);
+  const [wishlist, setWishlist] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:5555/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("Error fetching books:", error));
+  }, []);
+
+  
+
+  
+
   return (
-    <div>AllBooks</div>
-  )
+    <div>
+      <h1>All Books</h1>
+      
+    </div>
+  );
 }
 
-export default AllBooks
+export default AllBooks;
